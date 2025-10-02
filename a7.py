64e3bd3c25686583695bb1240ec6c2df6b2ce184
Guy-3322 <rpreiswerck@gmail.com>
2025-10-02 09:28:50 -0700

    done
    done

--- a7.py
+++ a7.py
@@ -11,3 +11,11 @@ Read through the file **example1.py** for information on using the math module.
 * You will require the use of the round() function from example1.py
 '''
 
+F=62.5
+
+F=((F - 32) / 1.8)
+
+numDecimals = 1
+y=round(F,numDecimals)
+
+print(y)
